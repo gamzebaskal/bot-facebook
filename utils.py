236a5e2a38d4d2d475855e141e516d609d038286
gamzebaskal <92956434+gamzebaskal@users.noreply.github.com>
2021-11-01 22:31:37 +0300

    Tanım güncellemeleri
    1-Kullanılan fonksiyonların detaylı açıklamaları yazıldı.
    2-Requirements dosyası eklendi.
    3-Girilen facebook sayfasının login sayfası olup olmadığını anlamak için gerekli fonksiyon yazıldı.
    4-Readme dosyası güncellendi.

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
 import hashlib
 import settings as s
 
-def hash_url(url):
+
+def hash_url(url: str):
+    """
+    Bağlantı sağlanacak URL değerini alır, md5 algoritması ile şifreleyip
+    url-md5.csv dosyasına kaydeder.
+    :param url: md5 ile şifrelenecek URL adresi
+    :return: md5 ile şifrelenmiş URL değeri
+    """
     md5_url = hashlib.md5(f"{url}".encode()).hexdigest()
     try:
         with open("url-md5.csv", "r+") as f:
-            print("break-")
 
             for i in f:
                 if i.strip() != md5_url:
